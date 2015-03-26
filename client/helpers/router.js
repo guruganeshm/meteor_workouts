8d300688dcbba7f79e658ff6b6d094680620c031
guruganeshm <guru96299@gmail.com>
2015-03-27 03:09:31 +0530

    Added new page

---
+++ client/helpers/router.js
@@ -0,0 +1,8 @@
+Router.map(function(){
+	this.route('playersList', {path:'/'});
+
+	this.route('playerDetails', {
+		path: 'players/:id',
+		data: function() { return Players.findone(this.params._id);}
+	});
+});
